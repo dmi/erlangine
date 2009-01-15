3a8c4a366659fe4703a85be9ffbaf40763f2ccda
Dmitry Chernyak <losthost@narod.ru>
2009-01-15 14:07:12 +0300

    Implemented simple logging
    When running start.sh, log going into log/enge2.log
    Interactive shell may be obtained using:
    
    erl -sname myshell -remsh enge2@localhost
    ...type commands
    ...exit with ^C^C

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s enge2
+# pretty dirty ;-) TODO: implement OTP logging
+erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -sname enge2@localhost -s enge2 -noinput -mnesia dir db >>$PWD/log/enge2.log 2>&1
