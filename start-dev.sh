6a9786468b4d5b74328698a871b1e2858af97cad
Dmitry Chernyak <dmi@cityship.feautec.pp.ru>
2009-01-17 00:24:59 +0300

    Reloader support added to start.sh

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -sname enge2@localhost -s enge2 -mnesia dir db
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s enge2 -mnesia dir db -sname enge2@localhost
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 # pretty dirty ;-) TODO: implement OTP logging
-erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -sname enge2@localhost -s enge2 -noinput -mnesia dir db >>$PWD/log/enge2.log 2>&1
+erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s enge2 -noinput -mnesia dir db -sname enge2@localhost >>$PWD/log/enge2.log 2>&1
