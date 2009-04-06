0ccae4be40298b0113372af28be8c52afcc0c28d
Dmitry Chernyak <losthost@narod.ru>
2009-04-06 19:44:47 +0400

    all is broken. I goin home :-)

--- src/ajax_entry.erl
+++ src/ajax_entry.erl
@@ -3,21 +3,32 @@
 -include("session.hrl").
 -include("authkey.hrl").
 
-entry_types(_Struct, _Session, _Req) ->
-    {{ok, [{entry, [<<"Запись">>, <<"Кафе">>]},
-           {fields, [string, text]}]},
+entry_templates(_Struct, _Session, _Req) ->
+    {{ok, [{entry, [<<"Запись">>, <<"Кафе">>]}]},
      []}.
 
 template(<<"Запись">>, _Session, _Req) ->
-    {{ok, [{fields, [[{name, <<"Название">>},
-                      {type, string}]]}]},
+    {{ok, [{fields, [[{name, <<"Название">>}]]}]},
      []};
 template(<<"Кафе">>, _Session, _Req) ->
-    {{ok, [{fields, [[{name, <<"Название">>},
-                      {type, string}],
-                     [{name, test},
-                      {type, text},
-                      {value, blabla}]]}]},
+    {{ok, [{fields, [[{name, <<"Описание">>},
+                      {values, [[{type, <<"Название">>}, {value, <<"Портос">>}],
+                                [{type, <<"Тип">>}, {value, <<"Кафе">>}]]}],
+                     [{name, <<"Место">>},
+                      {values,
+                          [[{type, <<"Город">>}, {value, <<"Санкт-Петербург">>}],
+                           [{type, <<"Адрес">>}, {value, <<"Кузнечный пер.">>}],
+                           [{type, <<"Гео">>}]]}],
+                     [{name, <<"Бизнес-ланч">>},
+                      {values, [[{type, <<"Описание">>}, {value, <<"Стабильно приличная пища, среднего объема, не жирная. Можно длительно ходить">>}],
+                                [{type, <<"Оценка">>}, {value, <<"Хороший">>}]]}],
+                     [{name, <<"Коктейль">>},
+                      {values,
+                           [[{type, <<"Оценка">>}, {value, <<"Хороший">>}]]}]]}]},
      []};
 template(Any, _Session, _Req) ->
     {{fail, Any}, []}.
+
+value_types(_Struct, _Session, _Req) ->
+    {{ok, [string, text]},
+     []}.
