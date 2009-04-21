5944ada1a7284981eebd3c47fbe5cda3cfdca508
Dmitry Chernyak <losthost@narod.ru>
2009-04-21 18:18:17 +0400

    Changed startup scripts

--- start-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s enge2 -mnesia dir db -sname enge2@localhost
-#exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -mnesia dir db -sname enge2@localhost
--- start.sh
+++ start.sh
@@ -1,4 +1,29 @@
 #!/bin/sh
+
+node="enge2@localhost"
+
+enge_cmd="erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -mnesia dir db -sname $node"
+
 cd `dirname $0`
-# pretty dirty ;-) TODO: implement OTP logging
-erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s enge2 -noinput -mnesia dir db -sname enge2@localhost >>$PWD/log/enge2.log 2>&1
+case $1 in
+	prod) 
+		# production mode (background, pretty dirty ;-) TODO: implement OTP logging
+		nohup $enge_cmd -s enge2 -noinput >>$PWD/log/enge2.log 2>&1 &
+	;;
+	ctl)
+		# attach to working instance (exit: c-c c-c)
+		erl -sname ctl -remsh $node
+	;;
+	maint)
+		# maintenance mode (not starting enge2)
+		exec $enge_cmd
+	;;
+	dev)
+		# development mode (foreground)
+		exec $enge_cmd -s enge2
+	;;
+	*)
+		echo usage: $0 [prod ctl maint dev]
+		exit 1
+	;;
+esac
