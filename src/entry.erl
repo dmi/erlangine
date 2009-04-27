e7ed8a0274463ccd843e243dcefa3d69395d8d91
Dmitry Chernyak <dmi@cityship.feautec.pp.ru>
2009-04-28 01:20:52 +0400

    search improvement

--- src/entry.erl
+++ src/entry.erl
@@ -231,14 +231,20 @@ function(doc){
             else return undefined
         }).filter(test)[0]
     };
+    var simplify = function(str){
+        if(typeof(str) == 'string')
+            return str.substr(0, 50).toLowerCase()
+        else return str
+    };
     /******* library end *******/
     if(doc.type == 'entry')
         var title = extract_field(doc, 'Название');
         doc.fields.forEach(function(field){
             field.values.forEach(function(value){
-                if((value.value.length < 50) &&
-                   (value.value.split(/\s+/).length < 4))
-                  emit([field.name, value.value],
+                if(//(value.value.length < 50) &&
+                   //(value.value.split(/\s+/).length < 4))
+                   true)
+                  emit([field.name.toLowerCase(), simplify(value.value)],
                        {'rev': doc._rev, 'date': doc.date, 'destination': doc.destination, 'author': doc.author,
                         'title': extract_field(doc, 'Название'), 'type': extract_values(doc, 'Тип')})
             })
